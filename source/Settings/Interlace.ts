780a769ed3195567beadc88973b70244a0c4ddce
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-27 11:34:32 +0300

    feature. Setting interlace

---
+++ source/Settings/Interlace.ts
@@ -0,0 +1,29 @@
+import Setting from "./Setting";
+
+export enum Type {
+	none = "none",
+	line = "line",
+	plane = "plane",
+	partition = "partition",
+	JPEG = "JPEG",
+	GIF = "GIF",
+	PNG = "PNG",
+}
+
+export class Interlace implements Setting {
+	private readonly type: Type;
+
+	constructor(type: Type) {
+		this.type = type;
+	}
+
+	build(): string[] {
+		return ['-interlace', this.type];
+	}
+
+	inspect(): string {
+		return this.build().join(' ');
+	}
+}
+
+export default Interlace;
--- source/Settings/index.ts
+++ source/Settings/index.ts
@@ -1,5 +1,6 @@
 export {Setting} from './Setting';
 export {Fill} from './Fill';
 export {Font} from './Font';
+export {Interlace, Type as InterlaceType} from './Interlace';
 export {Monitor} from './Monitor';
 export {Size} from './Size';
