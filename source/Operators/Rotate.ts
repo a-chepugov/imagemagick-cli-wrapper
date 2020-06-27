e3ef8387caff50d149bf2da4b7c43bc1f8dd204e
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-27 11:21:58 +0300

    feature. Operator rotate

---
+++ source/Operators/Rotate.ts
@@ -0,0 +1,26 @@
+import Operator from "./Operator";
+
+export enum Shear {
+	">" = ">",
+	"<" = "<",
+}
+
+export class Rotate implements Operator {
+	private readonly degrees: number;
+	private readonly shear: Shear;
+
+	constructor(degrees: number, shear?: Shear) {
+		this.degrees = degrees;
+		this.shear = shear;
+	}
+
+	build(): string[] {
+		return ['-rotate', `${this.degrees}${this.shear ? this.shear : ''}`];
+	}
+
+	inspect(): string {
+		return this.build().join(' ');
+	}
+}
+
+export default Rotate;
--- source/Operators/index.ts
+++ source/Operators/index.ts
@@ -1,3 +1,4 @@
 export {Operator} from './Operator';
+export {Rotate, Shear as RotateShear} from './Rotate';
 export {Resize} from './Resize';
 
