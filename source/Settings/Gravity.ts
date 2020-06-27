0f2a2e2f64d31df492dc51be2ac9dc599b403fee
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-27 12:10:07 +0300

    feature. Setting gravity

---
+++ source/Settings/Gravity.ts
@@ -0,0 +1,33 @@
+import Setting from "./Setting";
+
+export enum Type {
+	None = "None",
+	Center = "Center",
+	East = "East",
+	Forget = "Forget",
+	NorthEast = "NorthEast",
+	North = "North",
+	NorthWest = "NorthWest",
+	SouthEast = "SouthEast",
+	South = "South",
+	SouthWest = "SouthWest",
+	West = "West",
+}
+
+export class Gravity implements Setting {
+	private readonly type: Type;
+
+	constructor(type: Type) {
+		this.type = type;
+	}
+
+	build(): string[] {
+		return ['-gravity', `${this.type}`];
+	}
+
+	inspect(): string {
+		return this.build().join(' ');
+	}
+}
+
+export default Gravity;
--- source/Settings/index.ts
+++ source/Settings/index.ts
@@ -1,6 +1,7 @@
 export {Setting} from './Setting';
 export {Fill} from './Fill';
 export {Font} from './Font';
+export {Gravity, Type as GravityType} from './Gravity';
 export {Interlace, Type as InterlaceType} from './Interlace';
 export {Monitor} from './Monitor';
 export {Quality} from './Quality';
